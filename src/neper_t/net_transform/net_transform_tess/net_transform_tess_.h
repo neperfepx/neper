953c0d4d9345f81e26e2397a2b22b09f09ca75bc
Romain QUEY <quey@emse.fr>
2019-08-26 15:41:13 +0200

    Clean net_transform
    Split directory into net_transform_tess and net_transform_tesr.

--- src/neper_t/net_transform/net_transform_tess/net_transform_tess_.h
+++ src/neper_t/net_transform/net_transform_tess/net_transform_tess_.h
@@ -5,10 +5,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include"../structIn_t.h"
+#include"structIn_t.h"
 #include"neut_t.h"
 
-#include"net_transform.h"
+#include"net_transform_tess.h"
 
-#include"net_transform_tess/net_transform_tess_.h"
-#include"net_transform_tesr/net_transform_tesr_.h"
+#include"net_transform_tess_cut/net_transform_tess_cut.h"
