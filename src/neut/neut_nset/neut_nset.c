neperfepx/neper

commit a4b747247f8e372d35f3b895f8ab95e13aa67c32

Update ut
- New version of ut cleaned and documented with Doxygen.
- Neper code cleaned accordingly.

VERSIONS
@@ -1,4 +1,4 @@
-New in 3.5.2-34 (20 Feb 2020):
+New in 3.5.2-35 (02 Mar 2020):
 - module -T: fixed -morphooptiobjective, fixed -statface nfaces, added
   tessellation keys nfaces_samedomain, nfacenb_samedomain, npolys_samedomain
   and npolynb_samedomain, fixed -transform planecut, added -transform

doc/texinfo/version.texi
@@ -3,9 +3,9 @@
 @c See the COPYING file in the top-level directory.
 
 @iftex
-@set NEPER_VERSION 3.5.2-34
+@set NEPER_VERSION 3.5.2-35
 @end iftex
 
 @ifinfo
-@set NEPER_VERSION 3.5.2-34
+@set NEPER_VERSION 3.5.2-35
 @end ifinfo

src/CMakeLists.txt
@@ -5,7 +5,7 @@
 cmake_minimum_required(VERSION 2.8)
 project(neper)
 
-set (NEPER_VERSION \"3.5.2-34\")
+set (NEPER_VERSION \"3.5.2-35\")
 
 # should we use the optional libraries?
 set (HAVE_GSL ON)
@@ -31,10 +31,10 @@ set (MUPARSER_BINARY_DIR "${PROJECT_BINARY_DIR}/contrib/muparser")
 set (MUPARSER_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/contrib/muparser/include")
 
 include_directories("${PROJECT_SOURCE_DIR}/contrib/openGJK")
-include_directories("${PROJECT_SOURCE_DIR}/contrib/ut")
-set (UT_SOURCE_DIR "${PROJECT_SOURCE_DIR}/contrib/ut")
-set (UT_BINARY_DIR "${PROJECT_BINARY_DIR}/contrib/ut")
-set (UT_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/contrib/ut/include")
+include_directories("${PROJECT_SOURCE_DIR}/contrib/ut/src")
+set (UT_SOURCE_DIR "${PROJECT_SOURCE_DIR}/contrib/ut/src")
+set (UT_BINARY_DIR "${PROJECT_BINARY_DIR}/contrib/ut/src")
+set (UT_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/contrib/ut/src/include")
 
 include_directories("${PROJECT_SOURCE_DIR}/contrib/orilib")
 include_directories("${PROJECT_SOURCE_DIR}/neut")
@@ -44,7 +44,7 @@ include_directories("${PROJECT_SOURCE_DIR}/neper_v")
 
 include_directories("${PROJECT_BINARY_DIR}")
 include_directories("${PROJECT_BINARY_DIR}/contrib/openGJK")
-include_directories("${PROJECT_BINARY_DIR}/contrib/ut")
+include_directories("${PROJECT_BINARY_DIR}/contrib/ut/src")
 include_directories("${PROJECT_BINARY_DIR}/contrib/orilib")
 include_directories("${PROJECT_BINARY_DIR}/contrib/muparser")
 include_directories("${PROJECT_BINARY_DIR}/neut")
@@ -83,7 +83,7 @@ set(MESSAGE_QUIET ON)
 
 add_subdirectory(contrib/openGJK)
 add_subdirectory(contrib/muparser)
-add_subdirectory(contrib/ut)
+add_subdirectory(contrib/ut/src)
 add_subdirectory(contrib/orilib)
 add_subdirectory(neut)
 add_subdirectory(neper_t)

src/neut/neut_nset/neut_nset.c
@@ -6,20 +6,20 @@
 
 void
 neut_nset_expand (struct NSET NSet0D, struct NSET NSet1D, struct NSET NSet2D,
-		  char *in, char **pout)
+                  char *in, char **pout)
 {
   int i, j;
   int qty;
   char **list = NULL;
 
-  ut_free_1d_char (*pout);
+  ut_free_1d_char (pout);
   (*pout) = ut_alloc_1d_char (1);
   (*pout)[0] = '\0';
 
   if (!in || strlen (in) == 0 || !strcmp (in, "none"))
     return;
 
-  if (ut_string_finds (in, "default") != -1)
+  if (strstr (in, "default"))
   {
     qty = 1;
     list = ut_alloc_2d_char (qty, 11);
@@ -30,10 +30,10 @@ neut_nset_expand (struct NSET NSet0D, struct NSET NSet1D, struct NSET NSet2D,
     else if (NSet0D.qty > 0)
       strcpy (list[0], "vertices");
     else
-      ut_error_reportbug ();
+      ut_print_neperbug ();
   }
 
-  else if (ut_string_finds (in, "all") != -1)
+  else if (strstr (in, "all"))
   {
     if (NSet2D.qty > 0)
     {
@@ -62,90 +62,90 @@ neut_nset_expand (struct NSET NSet0D, struct NSET NSet1D, struct NSET NSet2D,
   }
 
   else
-    ut_string_separate (in, NEUT_SEP_NODEP, &list, &qty);
+    ut_list_break (in, NEUT_SEP_NODEP, &list, &qty);
 
   for (i = 0; i < qty; i++)
   {
     if (!strcmp (list[i], "faces"))
     {
       for (j = 1; j <= NSet2D.qty; j++)
-	if (ut_string_finds (NSet2D.names[j], "body") == -1)
-	{
-	  (*pout) =
-	    ut_realloc_1d_char ((*pout),
-				strlen ((*pout)) + strlen (NSet2D.names[j]) +
-				2);
-	  if (strlen ((*pout)) > 0)
-	    (*pout) = strcat ((*pout), ",");
-	  (*pout) = strcat ((*pout), NSet2D.names[j]);
-	}
+        if (strstr (NSet2D.names[j], "body"))
+        {
+          (*pout) =
+            ut_realloc_1d_char ((*pout),
+                                strlen ((*pout)) + strlen (NSet2D.names[j]) +
+                                2);
+          if (strlen ((*pout)) > 0)
+            (*pout) = strcat ((*pout), ",");
+          (*pout) = strcat ((*pout), NSet2D.names[j]);
+        }
     }
     else if (!strcmp (list[i], "facebodies"))
     {
       for (j = 1; j <= NSet2D.qty; j++)
-	if (ut_string_finds (NSet2D.names[j], "body") != -1)
-	{
-	  (*pout) =
-	    ut_realloc_1d_char ((*pout),
-				strlen ((*pout)) + strlen (NSet2D.names[j]) +
-				2);
-	  if (strlen ((*pout)) > 0)
-	    (*pout) = strcat ((*pout), ",");
-	  (*pout) = strcat ((*pout), NSet2D.names[j]);
-	}
+        if (strstr (NSet2D.names[j], "body"))
+        {
+          (*pout) =
+            ut_realloc_1d_char ((*pout),
+                                strlen ((*pout)) + strlen (NSet2D.names[j]) +
+                                2);
+          if (strlen ((*pout)) > 0)
+            (*pout) = strcat ((*pout), ",");
+          (*pout) = strcat ((*pout), NSet2D.names[j]);
+        }
     }
     else if (!strcmp (list[i], "edges"))
     {
       for (j = 1; j <= NSet1D.qty; j++)
-	if (ut_string_finds (NSet1D.names[j], "body") == -1)
-	{
-	  (*pout) =
-	    ut_realloc_1d_char ((*pout),
-				strlen ((*pout)) + strlen (NSet1D.names[j]) +
-				2);
-	  if (strlen ((*pout)) > 0)
-	    (*pout) = strcat ((*pout), ",");
-	  (*pout) = strcat ((*pout), NSet1D.names[j]);
-	}
+        if (strstr (NSet1D.names[j], "body"))
+        {
+          (*pout) =
+            ut_realloc_1d_char ((*pout),
+                                strlen ((*pout)) + strlen (NSet1D.names[j]) +
+                                2);
+          if (strlen ((*pout)) > 0)
+            (*pout) = strcat ((*pout), ",");
+          (*pout) = strcat ((*pout), NSet1D.names[j]);
+        }
     }
     else if (!strcmp (list[i], "edgebodies"))
     {
       for (j = 1; j <= NSet1D.qty; j++)
-	if (ut_string_finds (NSet1D.names[j], "body") != -1)
-	{
-	  (*pout) =
-	    ut_realloc_1d_char ((*pout),
-				strlen ((*pout)) + strlen (NSet1D.names[j]) +
-				2);
-	  if (strlen ((*pout)) > 0)
-	    (*pout) = strcat ((*pout), ",");
-	  (*pout) = strcat ((*pout), NSet1D.names[j]);
-	}
+        if (strstr (NSet1D.names[j], "body"))
+        {
+          (*pout) =
+            ut_realloc_1d_char ((*pout),
+                                strlen ((*pout)) + strlen (NSet1D.names[j]) +
+                                2);
+          if (strlen ((*pout)) > 0)
+            (*pout) = strcat ((*pout), ",");
+          (*pout) = strcat ((*pout), NSet1D.names[j]);
+        }
     }
     else if (!strcmp (list[i], "vertices"))
     {
       for (j = 1; j <= NSet0D.qty; j++)
       {
-	(*pout) =
-	  ut_realloc_1d_char ((*pout),
-			      strlen ((*pout)) + strlen (NSet0D.names[j]) +
-			      2);
-	if (strlen ((*pout)) > 0)
-	  (*pout) = strcat ((*pout), ",");
-	(*pout) = strcat ((*pout), NSet0D.names[j]);
+        (*pout) =
+          ut_realloc_1d_char ((*pout),
+                              strlen ((*pout)) + strlen (NSet0D.names[j]) +
+                              2);
+        if (strlen ((*pout)) > 0)
+          (*pout) = strcat ((*pout), ",");
+        (*pout) = strcat ((*pout), NSet0D.names[j]);
       }
     }
     else
     {
       (*pout) =
-	ut_realloc_1d_char ((*pout), strlen ((*pout)) + strlen (list[i]) + 2);
+        ut_realloc_1d_char ((*pout), strlen ((*pout)) + strlen (list[i]) + 2);
       if (strlen ((*pout)) > 0)
-	(*pout) = strcat ((*pout), ",");
+        (*pout) = strcat ((*pout), ",");
       (*pout) = strcat ((*pout), list[i]);
     }
   }
 
-  ut_free_2d_char (list, qty);
+  ut_free_2d_char (&list, qty);
 
   return;
 }
@@ -164,34 +164,32 @@ neut_nset_set_zero (struct NSET *pNSet)
 void
 neut_nset_free (struct NSET *pNSet)
 {
-  ut_free_1d_int ((*pNSet).nodeqty);
+  ut_free_1d_int (&(*pNSet).nodeqty);
   if ((*pNSet).qty > 0)
   {
-    ut_free_2d_char ((*pNSet).names, (*pNSet).qty + 1);
-    ut_free_2d_int ((*pNSet).nodes, (*pNSet).qty + 1);
+    ut_free_2d_char (&(*pNSet).names, (*pNSet).qty + 1);
+    ut_free_2d_int (&(*pNSet).nodes, (*pNSet).qty + 1);
   }
 
   return;
 }
 
 void
-neut_nsets_inter (struct NSET NSet, int id1, int id2,
-		  char **pname, int **pnodes, int *pnodeqty)
+neut_nsets_inter (struct NSET NSet, int id1, int id2, char **pname,
+                  int **pnodes, int *pnodeqty)
 {
   if (pname != NULL)
   {
-    (*pname)
-      =
+    (*pname) =
       ut_alloc_1d_char (strlen (NSet.names[id1]) + strlen (NSet.names[id2]) +
-			1);
+                        1);
     sprintf ((*pname), "%s%s", NSet.names[id1], NSet.names[id2]);
   }
 
   (*pnodes) = ut_alloc_1d_int (NSet.nodeqty[id1] + NSet.nodeqty[id2]);
 
-  ut_array_1d_int_inter (NSet.nodes[id1], NSet.nodeqty[id1],
-			 NSet.nodes[id2], NSet.nodeqty[id2],
-			 (*pnodes), pnodeqty);
+  ut_array_1d_int_inter (NSet.nodes[id1], NSet.nodeqty[id1], NSet.nodes[id2],
+                         NSet.nodeqty[id2], (*pnodes), pnodeqty);
 
   (*pnodes) = ut_realloc_1d_int ((*pnodes), (*pnodeqty));
 

src/contrib/ut/CMakeLists.txt
@@ -1,151 +1,0 @@
-# Copyright (C) 2003-2019, Romain Quey
-# see the COPYING file in the top-level directory.
-
-cmake_minimum_required(VERSION 2.6)
-cmake_policy(SET CMP0012 NEW)
-project(ut)
-
-set (UT_VERSION_MAJOR 1)
-set (UT_VERSION_MINOR 0)
-set (UT_VERSION_PATCH 0)
-
-# should we use the optional libraries?
-option (HAVE_GSL      "Use the GNU Scientific Library (GSL)" ON)
-option (HAVE_MUPARSER "Use muparser"                         OFF)
-option (HAVE_OPENGJK  "Use openGJK"                          OFF)
-option (HAVE_NLOPT    "Use NLopt"                            OFF)
-option (HAVE_OPENMP   "Use OpenMP"                           ON)
-option (HAVE_PNG      "Use the libpng library"               ON)
-option (DEVEL_OPTIMIZATION "Enable optimization (ON for production use)" ON)
-option (DEVEL_DEBUGGING_FLAG "Enable debugging compilation flag (OFF for production use)" OFF)
-
-# configure a header file to pass some of the CMake settings
-# to the source code
-configure_file (
-  "${PROJECT_SOURCE_DIR}/ut_config.h.in"
-  "${PROJECT_BINARY_DIR}/ut_config.h"
-)
-
-include_directories(${PROJECT_SOURCE_DIR} ${PROJECT_BINARY_DIR})
-
-if (HAVE_MUPARSER)
-  if (DEFINED MUPARSER_INCLUDE_DIR AND DEFINED MUPARSER_BINARY_DIR)
-    include_directories(${MUPARSER_INCLUDE_DIR} ${MUPARSER_BINARY_DIR})
-  elseif (EXISTS ${PROJECT_SOURCE_DIR}/contrib/muparser)
-      include_directories(${PROJECT_SOURCE_DIR}/contrib/muparser
-                          ${PROJECT_BINARY_DIR}/contrib/muparser)
-      add_subdirectory(contrib/muparser)
-  else ()
-    message (FATAL_ERROR "Failed to find muparser")
-  endif ()
-endif ()
-
-if (HAVE_OPENGJK)
-  include_directories(${OPENGJK_INCLUDE_DIR} ${OPENGJK_BINARY_DIR})
-endif ()
-
-add_definitions(-Wall -W)
-if (DEVEL_OPTIMIZATION)
-  add_definitions(-O3)
-endif ()
-
-if (DEVEL_DEBUGGING_FLAG)
-  add_definitions(-g -Werror)
-endif ()
-
-set (CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS})
-
-if (NOT DEFINED BUILD_SHARED_LIBS)
-  set (BUILD_SHARED_LIBS ON)
-endif ()
-
-add_library (ut
-  ut_alloc/ut_alloc.c
-  ut_arg/ut_arg.c
-  ut_array/ut_array.c
-  ut_data/ut_data.c
-  ut_color/ut_color.c
-  ut_file/ut_file.c
-  ut_free/ut_free.c
-  ut_math/ut_math.cpp
-  ut_fct/ut_fct.c
-  ut_stat/ut_stat.c
-  ut_mat/ut_mat.c
-  ut_linalg/ut_linalg.c
-  ut_num/ut_num.c
-  ut_option/ut_option.c
-  ut_print/ut_print.c
-  ut_space/ut_space.c
-  ut_space/ut_space_ellipse/ut_space_ellipse.c
-  ut_string/ut_string.c
-  ut_sys/ut_sys.c
-  ut_tensor/ut_tensor.c
-  ut_time/ut_time.c
-  ut_vector/ut_vector.c
-  ut_png/ut_png.c
-  ut_nlopt/ut_nlopt.c
-)
-
-## Link libraries
-
-set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake ${CMAKE_MODULE_PATH})
-
-find_library (MATHLIB m)
-if (MATHLIB)
-  set (EXTRA_LIBS ${EXTRA_LIBS} ${MATHLIB})
-endif (MATHLIB)
-
-if (HAVE_GSL)
-  find_package(GSL REQUIRED)
-  set (EXTRA_LIBS ${EXTRA_LIBS} ${GSL_LIBRARIES})
-  include_directories (${GSL_INCLUDE_DIRS})
-endif (HAVE_GSL)
-
-if (HAVE_PNG)
-  find_package(PNG REQUIRED)
-  set (EXTRA_LIBS ${EXTRA_LIBS} ${PNG_LIBRARIES})
-  include_directories (${PNG_INCLUDE_DIRS})
-endif ()
-
-if (HAVE_OPENMP)
-  find_package (OpenMP REQUIRED)
-  add_definitions(${OpenMP_C_FLAGS})
-endif ()
-
-if (HAVE_MUPARSER)
-  set (EXTRA_LIBS ${EXTRA_LIBS} muparser)
-endif ()
-
-if (HAVE_OPENGJK)
-  set (EXTRA_LIBS ${EXTRA_LIBS} openGJKlib)
-endif ()
-
-target_link_libraries (ut ${EXTRA_LIBS})
-
-# Installation of the library
-SET (CMAKE_INSTALL_PREFIX /usr/local/lib/ut)
-SET (DESTDIR /usr/local/lib/ut)
-
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_alloc.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_arg.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_array.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_data.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_color.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_file.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_free.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_math.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_mat.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_linalg.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_num.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_option.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_print.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_space.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_string.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_sys.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_tensor.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_time.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_vector.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_nlopt.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut.h)
-set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_BINARY_DIR}/ut_config.h)
-

src/contrib/ut/src/CMakeLists.txt
@@ -1,0 +1,149 @@
+# Copyright (C) 2003-2019, Romain Quey
+# see the COPYING file in the top-level directory.
+
+cmake_minimum_required(VERSION 2.6)
+cmake_policy(SET CMP0012 NEW)
+project(ut)
+
+set (UT_VERSION_MAJOR 1)
+set (UT_VERSION_MINOR 0)
+set (UT_VERSION_PATCH 0)
+
+# should we use the optional libraries?
+option (HAVE_GSL      "Use the GNU Scientific Library (GSL)" ON)
+option (HAVE_MUPARSER "Use muparser"                         OFF)
+option (HAVE_OPENGJK  "Use openGJK"                          OFF)
+option (HAVE_NLOPT    "Use NLopt"                            OFF)
+option (HAVE_OPENMP   "Use OpenMP"                           ON)
+option (HAVE_PNG      "Use the libpng library"               ON)
+option (DEVEL_OPTIMIZATION "Enable optimization (ON for production use)" ON)
+option (DEVEL_DEBUGGING_FLAG "Enable debugging compilation flag (OFF for production use)" OFF)
+
+# configure a header file to pass some of the CMake settings
+# to the source code
+configure_file (
+  "${PROJECT_SOURCE_DIR}/ut_config.h.in"
+  "${PROJECT_BINARY_DIR}/ut_config.h"
+)
+
+include_directories(${PROJECT_SOURCE_DIR} ${PROJECT_BINARY_DIR})
+
+if (HAVE_MUPARSER)
+  if (DEFINED MUPARSER_INCLUDE_DIR AND DEFINED MUPARSER_BINARY_DIR)
+    include_directories(${MUPARSER_INCLUDE_DIR} ${MUPARSER_BINARY_DIR})
+  elseif (EXISTS ${PROJECT_SOURCE_DIR}/contrib/muparser)
+      include_directories(${PROJECT_SOURCE_DIR}/contrib/muparser
+                          ${PROJECT_BINARY_DIR}/contrib/muparser)
+      add_subdirectory(contrib/muparser)
+  else ()
+    message (FATAL_ERROR "Failed to find muparser")
+  endif ()
+endif ()
+
+if (HAVE_OPENGJK)
+  include_directories(${OPENGJK_INCLUDE_DIR} ${OPENGJK_BINARY_DIR})
+endif ()
+
+add_definitions(-Wall -W)
+if (DEVEL_OPTIMIZATION)
+  add_definitions(-O3)
+endif ()
+
+if (DEVEL_DEBUGGING_FLAG)
+  add_definitions(-g -Werror)
+endif ()
+
+set (CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS})
+
+if (NOT DEFINED BUILD_SHARED_LIBS)
+  set (BUILD_SHARED_LIBS ON)
+endif ()
+
+add_library (ut
+  ut_alloc/ut_alloc.c
+  ut_arg/ut_arg.c
+  ut_array/ut_array.c
+  ut_color/ut_color.c
+  ut_file/ut_file.c
+  ut_free/ut_free.c
+  ut_math/ut_math.cpp
+  ut_fct/ut_fct.c
+  ut_xfig/ut_xfig.c
+  ut_stat/ut_stat.c
+  ut_mat/ut_mat.c
+  ut_linalg/ut_linalg.c
+  ut_num/ut_num.c
+  ut_print/ut_print.c
+  ut_space/ut_space.c
+  ut_space/ut_space_ellipse/ut_space_ellipse.c
+  ut_string/ut_string.c
+  ut_list/ut_list.c
+  ut_sys/ut_sys.c
+  ut_tensor/ut_tensor.c
+  ut_time/ut_time.c
+  ut_vector/ut_vector.c
+  ut_png/ut_png.c
+  ut_nlopt/ut_nlopt.c
+)
+
+## Link libraries
+
+set(CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake ${CMAKE_MODULE_PATH})
+
+find_library (MATHLIB m)
+if (MATHLIB)
+  set (EXTRA_LIBS ${EXTRA_LIBS} ${MATHLIB})
+endif (MATHLIB)
+
+if (HAVE_GSL)
+  find_package(GSL REQUIRED)
+  set (EXTRA_LIBS ${EXTRA_LIBS} ${GSL_LIBRARIES})
+  include_directories (${GSL_INCLUDE_DIRS})
+endif (HAVE_GSL)
+
+if (HAVE_PNG)
+  find_package(PNG REQUIRED)
+  set (EXTRA_LIBS ${EXTRA_LIBS} ${PNG_LIBRARIES})
+  include_directories (${PNG_INCLUDE_DIRS})
+endif ()
+
+if (HAVE_OPENMP)
+  find_package (OpenMP REQUIRED)
+  add_definitions(${OpenMP_C_FLAGS})
+endif ()
+
+if (HAVE_MUPARSER)
+  set (EXTRA_LIBS ${EXTRA_LIBS} muparser)
+endif ()
+
+if (HAVE_OPENGJK)
+  set (EXTRA_LIBS ${EXTRA_LIBS} openGJKlib)
+endif ()
+
+target_link_libraries (ut ${EXTRA_LIBS})
+
+# Installation of the library
+SET (CMAKE_INSTALL_PREFIX /usr/local/lib/ut)
+SET (DESTDIR /usr/local/lib/ut)
+
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_alloc.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_arg.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_array.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_color.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_file.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_free.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_math.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_mat.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_linalg.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_num.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_print.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_space.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_string.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_sys.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_tensor.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_time.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_vector.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut_nlopt.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_SOURCE_DIR}/ut.h)
+set (INCLUDE_FILES ${INCLUDE_FILES} ${PROJECT_BINARY_DIR}/ut_config.h)
+

src/neper_v/CMakeLists.txt
@@ -42,7 +42,6 @@ add_library (neper_v
 ./nev_include/nev_include1.c
 ./nev_image/nev_image1.c
 ./neper_v.c
-./nev_processimage/nev_processimage1.c
 ./nev_load/nev_load1.c
 ./nev_load/nev_load2.c
 ./nev_data/nev_data1.c

commit 5fbde178672534b1ff6aa22289dcc56dd45669d5

Fix -nset
Fixed simple bug introduced in a4b7472.

Fix #51.  Thanks @MiguelVCarvalho for the report.

VERSIONS
@@ -1,4 +1,4 @@
-New in 3.5.2-38 (09 Mar 2020):
+New in 3.5.2-39 (10 Mar 2020):
 - module -T: fixed -morphooptiobjective, fixed -statface nfaces, added
   tessellation keys nfaces_samedomain, nfacenb_samedomain, npolys_samedomain
   and npolynb_samedomain, fixed -transform planecut, added -transform

doc/texinfo/version.texi
@@ -3,9 +3,9 @@
 @c See the COPYING file in the top-level directory.
 
 @iftex
-@set NEPER_VERSION 3.5.2-38
+@set NEPER_VERSION 3.5.2-39
 @end iftex
 
 @ifinfo
-@set NEPER_VERSION 3.5.2-38
+@set NEPER_VERSION 3.5.2-39
 @end ifinfo

src/CMakeLists.txt
@@ -5,7 +5,7 @@
 cmake_minimum_required(VERSION 2.8)
 project(neper)
 
-set (NEPER_VERSION \"3.5.2-38\")
+set (NEPER_VERSION \"3.5.2-39\")
 
 # should we use the optional libraries?
 set (HAVE_GSL ON)

src/neut/neut_nset/neut_nset.c
@@ -69,7 +69,7 @@ neut_nset_expand (struct NSET NSet0D, struct NSET NSet1D, struct NSET NSet2D,
     if (!strcmp (list[i], "faces"))
     {
       for (j = 1; j <= NSet2D.qty; j++)
-        if (strstr (NSet2D.names[j], "body"))
+        if (!strstr (NSet2D.names[j], "body"))
         {
           (*pout) =
             ut_realloc_1d_char ((*pout),
@@ -97,7 +97,7 @@ neut_nset_expand (struct NSET NSet0D, struct NSET NSet1D, struct NSET NSet2D,
     else if (!strcmp (list[i], "edges"))
     {
       for (j = 1; j <= NSet1D.qty; j++)
-        if (strstr (NSet1D.names[j], "body"))
+        if (!strstr (NSet1D.names[j], "body"))
         {
           (*pout) =
             ut_realloc_1d_char ((*pout),