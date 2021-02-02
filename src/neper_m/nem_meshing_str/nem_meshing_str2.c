03c25dadf45502be106f4d4333ad3383d4fa1017
Romain QUEY <quey@emse.fr>
2021-02-02 20:28:31 +0100

    Speed up hex meshing
    The existing algorithm for hex meshing was multithreaded, which reduces
    computation time by a factor of 2+.
    
    A more efficient algorithm would be needed for better performance.

--- src/neper_m/nem_meshing_str/nem_meshing_str2.c
+++ src/neper_m/nem_meshing_str/nem_meshing_str2.c
@@ -8,34 +8,61 @@ void
 nem_meshing_str_tess (struct TESS Tess, struct NODES *pNodes,
                       struct MESH *Mesh)
 {
-  int elt, pid, prevpid;
+  int i, elt, sum;
   char *progress = ut_alloc_1d_char (100);
-  double *coo = ut_alloc_1d (3);
 
   ut_print_progress (stdout, 0, Mesh[Tess.Dim].EltQty, "%3.0f%%", progress);
-  prevpid = 1;
+
+  sum = 0;
+#pragma omp parallel for schedule(dynamic) private(elt)
   for (elt = 1; elt <= Mesh[Tess.Dim].EltQty; elt++)
   {
-    neut_mesh_elt_centre (*pNodes, Mesh[Tess.Dim], elt, coo);
-    if (neut_tess_point_incell (Tess, coo, prevpid) == 1)
-      Mesh[Tess.Dim].EltElset[elt] = prevpid;
-    else
-      for (pid = 1; pid <= Tess.CellQty; pid++)
-      {
-        if (pid == prevpid)
-          continue;
+    int prevpid = 0, *prevpids = NULL, prevpidqty = 0, pid;
+    double *coo = ut_alloc_1d (3);
 
-        if (neut_tess_point_incell (Tess, coo, pid) == 1)
+    neut_mesh_elt_centre (*pNodes, Mesh[Tess.Dim], elt, coo);
+
+    Mesh[Tess.Dim].EltElset[elt] = 0;
+
+    // testing prevpid
+    if (prevpid > 0 && neut_tess_point_incell (Tess, coo, prevpid) == 1)
+      Mesh[Tess.Dim].EltElset[elt] = prevpid;
+
+    // testing prevpids
+    if (!Mesh[Tess.Dim].EltElset[elt] && prevpidqty)
+    {
+      for (i = 0; i < prevpidqty; i++)
+        if (neut_tess_point_incell (Tess, coo, prevpids[i]) == 1)
         {
-          Mesh[Tess.Dim].EltElset[elt] = pid;
-          prevpid = pid;
+          Mesh[Tess.Dim].EltElset[elt] = prevpids[i];
+          prevpid = prevpids[i];
           break;
         }
-      }
+    }
 
-    /* print percent evolution in case of verbosity */
-    ut_print_progress (stdout, elt, Mesh[Tess.Dim].EltQty, "%3.0f%%",
+    // testing other ids
+    if (!Mesh[Tess.Dim].EltElset[elt])
+    {
+      for (pid = 1; pid <= Tess.CellQty; pid++)
+        if (pid != prevpid && ut_array_1d_int_eltpos (prevpids, prevpidqty, pid) == -1)
+          if (neut_tess_point_incell (Tess, coo, pid) == 1)
+          {
+            Mesh[Tess.Dim].EltElset[elt] = pid;
+            prevpid = pid;
+            ut_array_1d_int_list_addval (&prevpids, &prevpidqty, pid);
+            break;
+          }
+    }
+
+#pragma omp critical
+    sum++;
+
+#pragma omp critical
+    ut_print_progress (stdout, sum, Mesh[Tess.Dim].EltQty, "%3.0f%%",
                        progress);
+
+    ut_free_1d (&coo);
+    ut_free_1d_int (&prevpids);
   }
 
   neut_mesh_init_elsets (Mesh + Tess.Dim);
@@ -44,7 +71,6 @@ nem_meshing_str_tess (struct TESS Tess, struct NODES *pNodes,
   neut_nodes_rmorphans (pNodes, Mesh + Tess.Dim, NULL);
 
   ut_free_1d_char (&progress);
-  ut_free_1d (&coo);
 
   return;
 }
