0f0d06152a7c8ddf7c76176b32d60335f245c7fa
Romain QUEY <quey@emse.fr>
2021-11-22 16:41:49 +0100

    Add links to external modules
    Added links at the bottom of Downloads.

--- _downloads/c67040ccab0fd765031af6feca801764/gene_peri.sh
+++ _downloads/c67040ccab0fd765031af6feca801764/gene_peri.sh
@@ -2,9 +2,9 @@
 
 NEPER="neper --rcfile none"
 
-# $NEPER -T -n 1000 -morpho gg -per 1   -o gene_peri_1
-# $NEPER -T -n 1000 -morpho gg -per x   -o gene_peri_2
-# $NEPER -T -n 1000 -morpho gg -per x,y -o gene_peri_3
+$NEPER -T -n 1000 -morpho gg -per 1   -o gene_peri_1
+$NEPER -T -n 1000 -morpho gg -per x   -o gene_peri_2
+$NEPER -T -n 1000 -morpho gg -per x,y -o gene_peri_3
 
 C="-datacellcol id -dataedgerad 0.002 -datacelltrs 0.5 -cameraangle 14 -cameracoo x:-6.3:z -cameraprojection orthographic"
 for i in 1 2 3
