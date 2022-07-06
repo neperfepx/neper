6b5d22d8b479295f5982098ceef4984ced69b013
Romain QUEY <quey@emse.fr>
2022-07-06 08:37:43 +0200

    Fix FORCE_BUILTIN_LIBSCOTCH & FORCE_BUILTIN_NLOPT
    Fixed CMakeLists.txt so that the system files and directories are
    effetively forgotten when built-in is enforced.

--- doc/conf.py
+++ doc/conf.py
@@ -11,8 +11,8 @@ sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 
 project = u'Neper'
-version = u'4.4.2-24'
-release = u'4.4.2-24'
+version = u'4.4.2-25'
+release = u'4.4.2-25'
 author = u'Romain Quey'
 copyright = u'Romain Quey'
 language = 'en'
