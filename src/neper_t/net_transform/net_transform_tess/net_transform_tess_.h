48b34f4ddb2cf5150b2b41efdfec3d226150119b
Romain QUEY <quey@emse.fr>
2022-07-19 14:50:44 +0200

    Fix compilation warning
    Simple fix.

--- src/neper_t/net_transform/net_transform_tess/net_transform_tess_.h
+++ src/neper_t/net_transform/net_transform_tess/net_transform_tess_.h
@@ -11,5 +11,6 @@
 #include"net_transform_tess.h"
 
 #include"net_transform_tess_cut/net_transform_tess_cut.h"
+#include"net_ori/net_ori.h"
 
 extern void net_transform_tess_ori (char *ori, struct TESS *pTess);
