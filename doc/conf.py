de1324c52cd08489dee784f6f476dbd7d6164cde
Romain QUEY <quey@emse.fr>
2026-05-07 11:42:15 +0200

    Fix format_2d test
    Fixed reference svg file.

--- doc/conf.py
+++ doc/conf.py
@@ -11,8 +11,8 @@ sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 
 project = u'Neper'
-version = u'4.10.2-44'
-release = u'4.10.2-44'
+version = u'4.10.2-45'
+release = u'4.10.2-45'
 author = u'Romain Quey'
 copyright = u'Romain Quey'
 language = 'en'
